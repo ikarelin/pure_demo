aae0f28c57ce4d8b8584177c17728938c5ba0ae1
Igor Karelin <ikarelin@me.com>
2025-04-15 22:23:02 +0300

    Completed main screen UI.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -38,15 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -54,69 +31,227 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  bool privacyEnabled = false;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      backgroundColor: Color(0xFFD9D9D9),
+      body: Stack(
+        children: [
+          Positioned(bottom: 0, child: _bottomBar()),
+          Positioned(
+            bottom: 60,
+            child: Container(
+              height: MediaQuery.of(context).size.height - 230,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Color(0xFF212020),
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(10),
+                  topLeft: Radius.circular(10),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  _chatHeaderWidget(),
+
+                  _likesWidget(),
+                  SizedBox(height: 8),
+                  _chatListWidget(),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _chatHeaderWidget() {
+    return SizedBox(
+      height: 60,
+      child: Row(
+        children: [
+          SizedBox(width: 16),
+          Text('ЧАТЫ', style: TextStyle(fontSize: 20, color: Colors.white)),
+          Spacer(),
+          Text(
+            !privacyEnabled ? 'OFF' : 'ON',
+            style: TextStyle(fontSize: 16, color: Colors.white),
+          ),
+          SizedBox(width: 8),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                privacyEnabled = !privacyEnabled;
+              });
+            },
+            icon: Image.asset('assets/Swithers.png', width: 40, height: 20),
+          ),
+
+          SizedBox(width: 8),
+        ],
+      ),
+    );
+  }
+
+  Widget _likesWidget() {
+    return SizedBox(
+      height: 60,
+      child: Row(
+        children: [
+          SizedBox(width: 10),
+          SvgPicture.asset('assets/purple_heart.svg'),
+          SizedBox(width: 10),
+          Text(
+            '44 человека тебя лайкнули',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Spacer(),
+          Container(
+            height: 4,
+            width: 4,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          SizedBox(width: 16),
+        ],
+      ),
+    );
+  }
+
+  Widget _chatListWidget() {
+    return SingleChildScrollView(
+      child: ListView(
+        shrinkWrap: true,
+        padding: EdgeInsets.all(0),
+        children: [
+          _chatListTile(
+            'assets/user1.jpeg',
+            '23 ч 43 мин ',
+            'Отлично выглядишь',
+            false,
+          ),
+          SizedBox(height: 16),
+          _chatListTile(
+            'assets/user2.jpeg',
+            '20 ч 40 мин',
+            'Встретимся? Я рядом',
+            true,
+          ),
+          SizedBox(height: 16),
+          _chatListTile(
+            'assets/user3.jpeg',
+            '18 ч 40 мин',
+            'Встретимся?',
+            false,
+          ),
+          SizedBox(height: 16),
+          _chatListTile(
+            'assets/user4.jpeg',
+            '09 ч 40 мин',
+            'Давно тебя хочу',
+            false,
+          ),
+          SizedBox(height: 16),
+          _chatListTile(
+            'assets/user5.jpeg',
+            '03 ч 04 мин',
+            'Я в ванной... Скинь фото..',
+            false,
+          ),
+          SizedBox(height: 16),
+          _chatListTile('assets/user6.jpeg', '01 ч 09 мин', 'Привет', false),
+          // List item 2
+          // ...
+        ],
+      ),
+    );
+  }
+
+  Widget _chatListTile(
+    String picture,
+    String date,
+    String message,
+    bool unread,
+  ) {
+    return Container(
+      height: 60,
+      padding: EdgeInsets.all(0),
+      color: Color(0xFF212020),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 4, right: 4),
+            child: SizedBox(
+              width: 60,
+              height: 60,
+              child: CircleAvatar(backgroundImage: AssetImage(picture)),
+            ),
+          ),
+          SizedBox(width: 16),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(date, style: TextStyle(color: Colors.white, fontSize: 12)),
+              Text(
+                message,
+                style: TextStyle(color: Colors.white, fontSize: 14),
+              ),
+            ],
+          ),
+          if (unread == true) Spacer(),
+          if (unread == true)
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: Container(
+                height: 4,
+                width: 4,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget _bottomBar() {
+    return Container(
+      height: 60,
+      width: MediaQuery.of(context).size.width,
+      color: Color(0xFF191818),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset('assets/menu_left.png'),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset('assets/menu_chat.png'),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset('assets/menu_heart.png'),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset('assets/menu_settings.png'),
+          ),
+        ],
+      ),
     );
   }
 }
